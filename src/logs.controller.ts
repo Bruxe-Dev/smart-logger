6a6205931ef6bddfe22736279814124fd579249e
Bruxe-Dev <mnibeza23@gmail.com>
2026-05-05 20:49:52 +0200

    implemented my first Controller

--- src/logs.controller.ts
+++ src/logs.controller.ts
@@ -1,2 +1,15 @@
 import { Controller, Req, Res, Query, Body, Post, Get, Param } from "@nestjs/common";
-import { Request } from "express";
+import type { Request } from "express";
+
+let logs = [];
+let blockedIps = new Set()
+
+@Controller()
+export class LogsController {
+    @Post('track')
+    track(@Req() req: Request) {
+        if (blockedIps.has(req.ip)) {
+            return { status: 'rejected', message: 'Sorry, But your Ip is blocked' }
+        }
+    }
+}
